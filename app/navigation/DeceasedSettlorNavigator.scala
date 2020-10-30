432107ade3191cfe30c0d17a944109dc10e4f77e
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-10-30 16:12:23 +0000

    Deceased settlor navigator spec.

--- app/navigation/DeceasedSettlorNavigator.scala
+++ app/navigation/DeceasedSettlorNavigator.scala
@@ -17,7 +17,7 @@
 package navigation
 
 import config.FrontendAppConfig
-import controllers.routes
+import controllers.deceased_settlor.routes._
 import javax.inject.{Inject, Singleton}
 import models.{NormalMode, UserAnswers}
 import pages.Page
@@ -29,48 +29,45 @@ import uk.gov.hmrc.auth.core.AffinityGroup
 class DeceasedSettlorNavigator @Inject()(config: FrontendAppConfig) extends Navigator {
 
   override protected def route(draftId: String): PartialFunction[Page, AffinityGroup => UserAnswers => Call] = {
-    case SettlorsNamePage => _ => _ => controllers.deceased_settlor.routes.SettlorDateOfDeathYesNoController.onPageLoad(NormalMode, draftId)
-    case SettlorDateOfDeathYesNoPage => _ => deceasedSettlorDateOfDeathRoute(draftId)
-    case SettlorDateOfBirthYesNoPage => _ => deceasedSettlorDateOfBirthRoute(draftId)
-    case SettlorsDateOfBirthPage => _ => _ => controllers.deceased_settlor.routes.SettlorsNINoYesNoController.onPageLoad(NormalMode, draftId)
-    case SettlorsNationalInsuranceYesNoPage => _ => deceasedSettlorNinoRoute(draftId)
-    case SettlorsLastKnownAddressYesNoPage => _ => deceasedSettlorLastKnownAddressRoute(draftId)
-    case SettlorDateOfDeathPage => _ => _ => controllers.deceased_settlor.routes.SettlorDateOfBirthYesNoController.onPageLoad(NormalMode, draftId)
-    case SettlorNationalInsuranceNumberPage => _ => _ => controllers.deceased_settlor.routes.DeceasedSettlorAnswerController.onPageLoad(draftId)
-    case WasSettlorsAddressUKYesNoPage => _ => deceasedSettlorAddressRoute(draftId)
-    case SettlorsInternationalAddressPage => _ => _ => controllers.deceased_settlor.routes.DeceasedSettlorAnswerController.onPageLoad(draftId)
-    case SettlorsUKAddressPage => _ => _ => controllers.deceased_settlor.routes.DeceasedSettlorAnswerController.onPageLoad(draftId)
-    case DeceasedSettlorAnswerPage => _ => _ => Call("GET", config.registrationProgressUrl(draftId))
-  }
-
-  private def deceasedSettlorAddressRoute(draftId: String)(userAnswers: UserAnswers): Call = userAnswers.get(WasSettlorsAddressUKYesNoPage) match {
-    case Some(false) => controllers.deceased_settlor.routes.SettlorsInternationalAddressController.onPageLoad(NormalMode, draftId)
-    case Some(true) => controllers.deceased_settlor.routes.SettlorsUKAddressController.onPageLoad(NormalMode, draftId)
-    case _ => routes.SessionExpiredController.onPageLoad()
-  }
-
-  private def deceasedSettlorLastKnownAddressRoute(draftId: String)(userAnswers: UserAnswers): Call = userAnswers.get(SettlorsLastKnownAddressYesNoPage) match {
-    case Some(false) => controllers.deceased_settlor.routes.DeceasedSettlorAnswerController.onPageLoad(draftId)
-    case Some(true) => controllers.deceased_settlor.routes.WasSettlorsAddressUKYesNoController.onPageLoad(NormalMode, draftId)
-    case _ => routes.SessionExpiredController.onPageLoad()
-  }
-
-  private def deceasedSettlorNinoRoute(draftId: String)(userAnswers: UserAnswers): Call = userAnswers.get(SettlorsNationalInsuranceYesNoPage) match {
-    case Some(false) => controllers.deceased_settlor.routes.SettlorsLastKnownAddressYesNoController.onPageLoad(NormalMode, draftId)
-    case Some(true) => controllers.deceased_settlor.routes.SettlorNationalInsuranceNumberController.onPageLoad(NormalMode, draftId)
-    case _ => routes.SessionExpiredController.onPageLoad()
-  }
-
-  private def deceasedSettlorDateOfBirthRoute(draftId: String)(userAnswers: UserAnswers): Call = userAnswers.get(SettlorDateOfBirthYesNoPage) match {
-    case Some(false) => controllers.deceased_settlor.routes.SettlorsNINoYesNoController.onPageLoad(NormalMode, draftId)
-    case Some(true) => controllers.deceased_settlor.routes.SettlorsDateOfBirthController.onPageLoad(NormalMode, draftId)
-    case _ => routes.SessionExpiredController.onPageLoad()
-  }
-
-  private def deceasedSettlorDateOfDeathRoute(draftId: String)(userAnswers: UserAnswers): Call = userAnswers.get(SettlorDateOfDeathYesNoPage) match {
-    case Some(false) => controllers.deceased_settlor.routes.SettlorDateOfBirthYesNoController.onPageLoad(NormalMode, draftId)
-    case Some(true) => controllers.deceased_settlor.routes.SettlorDateOfDeathController.onPageLoad(NormalMode, draftId)
-    case _ => routes.SessionExpiredController.onPageLoad()
+    case SettlorsNamePage => _ => _ =>
+      controllers.deceased_settlor.routes.SettlorDateOfDeathYesNoController.onPageLoad(NormalMode, draftId)
+    case SettlorDateOfDeathYesNoPage => _ => yesNoNav(
+      fromPage = SettlorDateOfDeathYesNoPage,
+      yesCall = SettlorDateOfDeathController.onPageLoad(NormalMode, draftId),
+      noCall = SettlorDateOfBirthYesNoController.onPageLoad(NormalMode, draftId)
+    )
+    case SettlorDateOfDeathPage => _ => _ =>
+      controllers.deceased_settlor.routes.SettlorDateOfBirthYesNoController.onPageLoad(NormalMode, draftId)
+    case SettlorDateOfBirthYesNoPage => _ => yesNoNav(
+      fromPage = SettlorDateOfBirthYesNoPage,
+      yesCall = SettlorsDateOfBirthController.onPageLoad(NormalMode, draftId),
+      noCall = SettlorsNINoYesNoController.onPageLoad(NormalMode, draftId)
+    )
+    case SettlorsDateOfBirthPage => _ => _ =>
+      controllers.deceased_settlor.routes.SettlorsNINoYesNoController.onPageLoad(NormalMode, draftId)
+    case SettlorsNationalInsuranceYesNoPage => _ => yesNoNav(
+      fromPage = SettlorsNationalInsuranceYesNoPage,
+      yesCall = SettlorNationalInsuranceNumberController.onPageLoad(NormalMode, draftId),
+      noCall = SettlorsLastKnownAddressYesNoController.onPageLoad(NormalMode, draftId)
+    )
+    case SettlorNationalInsuranceNumberPage => _ => _ =>
+      controllers.deceased_settlor.routes.DeceasedSettlorAnswerController.onPageLoad(draftId)
+    case SettlorsLastKnownAddressYesNoPage => _ => yesNoNav(
+      fromPage = SettlorsLastKnownAddressYesNoPage,
+      yesCall = WasSettlorsAddressUKYesNoController.onPageLoad(NormalMode, draftId),
+      noCall = DeceasedSettlorAnswerController.onPageLoad(draftId)
+    )
+    case WasSettlorsAddressUKYesNoPage => _ => yesNoNav(
+      fromPage = WasSettlorsAddressUKYesNoPage,
+      yesCall = SettlorsUKAddressController.onPageLoad(NormalMode, draftId),
+      noCall = SettlorsInternationalAddressController.onPageLoad(NormalMode, draftId)
+    )
+    case SettlorsUKAddressPage => _ => _ =>
+      controllers.deceased_settlor.routes.DeceasedSettlorAnswerController.onPageLoad(draftId)
+    case SettlorsInternationalAddressPage => _ => _ =>
+      controllers.deceased_settlor.routes.DeceasedSettlorAnswerController.onPageLoad(draftId)
+    case DeceasedSettlorAnswerPage => _ => _ =>
+      Call("GET", config.registrationProgressUrl(draftId))
   }
 
 }
---
+++ test/navigation/DeceasedSettlorNavigatorSpec.scala
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigation
+
+import base.SpecBase
+import controllers.deceased_settlor.routes
+import models.{Mode, NormalMode}
+import pages.deceased_settlor._
+import play.api.mvc.Call
+
+class DeceasedSettlorNavigatorSpec extends SpecBase {
+
+  private val navigator: DeceasedSettlorNavigator = injector.instanceOf[DeceasedSettlorNavigator]
+  private val mode: Mode = NormalMode
+
+  "DeceasedSettlor Navigator" when {
+
+    "SettlorsNamePage" must {
+      "redirect to date of death yes/no" in {
+        navigator.nextPage(SettlorsNamePage, mode, fakeDraftId)(emptyUserAnswers)
+          .mustBe(routes.SettlorDateOfDeathYesNoController.onPageLoad(mode, fakeDraftId))
+      }
+    }
+
+    "SettlorDateOfDeathYesNoPage" when {
+
+      "yes" must {
+        "redirect to date of death" in {
+          val userAnswers = emptyUserAnswers.set(SettlorDateOfDeathYesNoPage, true).success.value
+
+          navigator.nextPage(SettlorDateOfDeathYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.SettlorDateOfDeathController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+
+      "no" must {
+        "redirect to date of birth yes/no" in {
+          val userAnswers = emptyUserAnswers.set(SettlorDateOfDeathYesNoPage, false).success.value
+
+          navigator.nextPage(SettlorDateOfDeathYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.SettlorDateOfBirthYesNoController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+    }
+
+    "SettlorDateOfDeathPage" must {
+      "redirect to date of birth yes/no" in {
+        navigator.nextPage(SettlorDateOfDeathPage, mode, fakeDraftId)(emptyUserAnswers)
+          .mustBe(routes.SettlorDateOfBirthYesNoController.onPageLoad(mode, fakeDraftId))
+      }
+    }
+
+    "SettlorDateOfBirthYesNoPage" when {
+
+      "yes" must {
+        "redirect to date of birth" in {
+          val userAnswers = emptyUserAnswers.set(SettlorDateOfBirthYesNoPage, true).success.value
+
+          navigator.nextPage(SettlorDateOfBirthYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.SettlorsDateOfBirthController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+
+      "no" must {
+        "redirect to NINO yes/no" in {
+          val userAnswers = emptyUserAnswers.set(SettlorDateOfBirthYesNoPage, false).success.value
+
+          navigator.nextPage(SettlorDateOfBirthYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.SettlorsNINoYesNoController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+    }
+
+    "SettlorsDateOfBirthPage" must {
+      "redirect to NINO yes/no" in {
+        navigator.nextPage(SettlorsDateOfBirthPage, mode, fakeDraftId)(emptyUserAnswers)
+          .mustBe(routes.SettlorsNINoYesNoController.onPageLoad(mode, fakeDraftId))
+      }
+    }
+
+    "SettlorsNationalInsuranceYesNoPage" when {
+
+      "yes" must {
+        "redirect to NINO" in {
+          val userAnswers = emptyUserAnswers.set(SettlorsNationalInsuranceYesNoPage, true).success.value
+
+          navigator.nextPage(SettlorsNationalInsuranceYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.SettlorNationalInsuranceNumberController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+
+      "no" must {
+        "redirect to last known address yes/no" in {
+          val userAnswers = emptyUserAnswers.set(SettlorsNationalInsuranceYesNoPage, false).success.value
+
+          navigator.nextPage(SettlorsNationalInsuranceYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.SettlorsLastKnownAddressYesNoController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+    }
+
+    "SettlorNationalInsuranceNumberPage" must {
+      "redirect to check answers" in {
+        navigator.nextPage(SettlorNationalInsuranceNumberPage, mode, fakeDraftId)(emptyUserAnswers)
+          .mustBe(routes.DeceasedSettlorAnswerController.onPageLoad(fakeDraftId))
+      }
+    }
+
+    "SettlorsLastKnownAddressYesNoPage" when {
+
+      "yes" must {
+        "redirect to address UK yes/no" in {
+          val userAnswers = emptyUserAnswers.set(SettlorsLastKnownAddressYesNoPage, true).success.value
+
+          navigator.nextPage(SettlorsLastKnownAddressYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.WasSettlorsAddressUKYesNoController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+
+      "no" must {
+        "redirect to check answers" in {
+          val userAnswers = emptyUserAnswers.set(SettlorsLastKnownAddressYesNoPage, false).success.value
+
+          navigator.nextPage(SettlorsLastKnownAddressYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.DeceasedSettlorAnswerController.onPageLoad(fakeDraftId))
+        }
+      }
+    }
+
+    "WasSettlorsAddressUKYesNoPage" when {
+
+      "yes" must {
+        "redirect to UK address" in {
+          val userAnswers = emptyUserAnswers.set(WasSettlorsAddressUKYesNoPage, true).success.value
+
+          navigator.nextPage(WasSettlorsAddressUKYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.SettlorsUKAddressController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+
+      "no" must {
+        "redirect to international address" in {
+          val userAnswers = emptyUserAnswers.set(WasSettlorsAddressUKYesNoPage, false).success.value
+
+          navigator.nextPage(WasSettlorsAddressUKYesNoPage, mode, fakeDraftId)(userAnswers)
+            .mustBe(routes.SettlorsInternationalAddressController.onPageLoad(mode, fakeDraftId))
+        }
+      }
+    }
+
+    "SettlorsUKAddressPage" must {
+      "redirect to check answers" in {
+        navigator.nextPage(SettlorsUKAddressPage, mode, fakeDraftId)(emptyUserAnswers)
+          .mustBe(routes.DeceasedSettlorAnswerController.onPageLoad(fakeDraftId))
+      }
+    }
+
+    "SettlorsInternationalAddressPage" must {
+      "redirect to  check answers" in {
+        navigator.nextPage(SettlorsInternationalAddressPage, mode, fakeDraftId)(emptyUserAnswers)
+          .mustBe(routes.DeceasedSettlorAnswerController.onPageLoad(fakeDraftId))
+      }
+    }
+
+    "DeceasedSettlorAnswerPage" must {
+      "redirect to task list" in {
+        navigator.nextPage(DeceasedSettlorAnswerPage, mode, fakeDraftId)(emptyUserAnswers)
+          .mustBe(Call("GET", frontendAppConfig.registrationProgressUrl(fakeDraftId)))
+      }
+    }
+  }
+}
