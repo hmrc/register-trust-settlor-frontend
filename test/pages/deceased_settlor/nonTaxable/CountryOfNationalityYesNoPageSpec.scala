c56e57a29ccd4228f6cad2bbadf986b7bfde2125
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2021-01-11 12:35:53 +0000

    CountryOfNationality

---
+++ test/pages/deceased_settlor/nonTaxable/CountryOfNationalityYesNoPageSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages.deceased_settlor.nonTaxable
+
+import java.time.LocalDate
+
+import models.UserAnswers
+import org.scalacheck.Arbitrary.arbitrary
+import pages.behaviours.PageBehaviours
+
+class CountryOfNationalityYesNoPageSpec extends PageBehaviours {
+
+  "CountryOfNationalityYesNoPage" must {
+
+    beRetrievable[Boolean](CountryOfNationalityYesNoPage)
+
+    beSettable[Boolean](CountryOfNationalityYesNoPage)
+
+    beRemovable[Boolean](CountryOfNationalityYesNoPage)
+  }
+
+//  "remove SettlorDateOfBirth when SettlorDateOfBirthYesNoPage is set to false" in {
+//    forAll(arbitrary[UserAnswers], arbitrary[String]) {
+//      (initial, str) =>
+//        val answers: UserAnswers = initial.set(SettlorsDateOfBirthPage, LocalDate.now()).success.value
+//        val result: UserAnswers = answers.set(SettlorDateOfBirthYesNoPage, false).success.value
+//
+//        result.get(SettlorsDateOfBirthPage) mustNot be (defined)
+//    }
+//  }
+}
