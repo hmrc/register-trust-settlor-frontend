a547113ac8378d1f823b6136ea05458becb628d7
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-01-13 14:48:58 +0000

    Navigator refactoring.

--- app/navigation/IndividualSettlorNavigator.scala
+++ app/navigation/IndividualSettlorNavigator.scala
@@ -31,22 +31,39 @@ class IndividualSettlorNavigator extends Navigator {
                         is5mldEnabled: Boolean = false): UserAnswers => Call = route(draftId, is5mldEnabled)(page)(af)
 
   override protected def route(draftId: String, fiveMld: Boolean): PartialFunction[Page, AffinityGroup => UserAnswers => Call] = {
+    simpleNavigation(draftId, fiveMld) orElse yesNoNavigation(draftId, fiveMld)
+  }
+
+  def simpleNavigation(draftId: String, fiveMld: Boolean): PartialFunction[Page, AffinityGroup => UserAnswers => Call] = {
     case SettlorIndividualNamePage(index) => _ => _ =>
       controllers.living_settlor.individual.routes.SettlorIndividualDateOfBirthYesNoController.onPageLoad(NormalMode, index, draftId)
+    case SettlorIndividualDateOfBirthPage(index) => _ => _ =>
+      controllers.living_settlor.individual.routes.SettlorIndividualNINOYesNoController.onPageLoad(NormalMode, index, draftId)
+    case SettlorIndividualNINOPage(index) => _ => _ =>
+      controllers.living_settlor.individual.routes.SettlorIndividualAnswerController.onPageLoad(index, draftId)
+    case SettlorAddressUKPage(index) => _ => _ =>
+      controllers.living_settlor.individual.routes.SettlorIndividualPassportYesNoController.onPageLoad(NormalMode, index, draftId)
+    case SettlorAddressInternationalPage(index) => _ => _ =>
+      controllers.living_settlor.individual.routes.SettlorIndividualPassportYesNoController.onPageLoad(NormalMode, index, draftId)
+    case SettlorIndividualPassportPage(index) => _ => _ =>
+      controllers.living_settlor.individual.routes.SettlorIndividualAnswerController.onPageLoad(index, draftId)
+    case SettlorIndividualIDCardPage(index) => _ => _ =>
+      controllers.living_settlor.individual.routes.SettlorIndividualAnswerController.onPageLoad(index, draftId)
+    case SettlorIndividualAnswerPage => _ => _ =>
+      controllers.routes.AddASettlorController.onPageLoad(draftId)
+  }
+
+  def yesNoNavigation(draftId: String, fiveMld: Boolean): PartialFunction[Page, AffinityGroup => UserAnswers => Call] = {
     case SettlorIndividualDateOfBirthYesNoPage(index)  => _ => yesNoNav(
       fromPage = SettlorIndividualDateOfBirthYesNoPage(index),
       yesCall = controllers.living_settlor.individual.routes.SettlorIndividualDateOfBirthController.onPageLoad(NormalMode, index, draftId),
       noCall = controllers.living_settlor.individual.routes.SettlorIndividualNINOYesNoController.onPageLoad(NormalMode, index, draftId)
     )
-    case SettlorIndividualDateOfBirthPage(index) => _ => _ =>
-      controllers.living_settlor.individual.routes.SettlorIndividualNINOYesNoController.onPageLoad(NormalMode, index, draftId)
     case SettlorIndividualNINOYesNoPage(index)  => _ => yesNoNav(
       fromPage = SettlorIndividualNINOYesNoPage(index),
       yesCall = controllers.living_settlor.individual.routes.SettlorIndividualNINOController.onPageLoad(NormalMode, index, draftId),
       noCall = controllers.living_settlor.individual.routes.SettlorIndividualAddressYesNoController.onPageLoad(NormalMode, index, draftId)
     )
-    case SettlorIndividualNINOPage(index) => _ => _ =>
-      controllers.living_settlor.individual.routes.SettlorIndividualAnswerController.onPageLoad(index, draftId)
     case SettlorAddressYesNoPage(index)  => _ => yesNoNav(
       fromPage = SettlorAddressYesNoPage(index),
       yesCall = controllers.living_settlor.individual.routes.SettlorIndividualAddressUKYesNoController.onPageLoad(NormalMode, index, draftId),
@@ -57,25 +74,15 @@ class IndividualSettlorNavigator extends Navigator {
       yesCall = controllers.living_settlor.individual.routes.SettlorIndividualAddressUKController.onPageLoad(NormalMode, index, draftId),
       noCall = controllers.living_settlor.individual.routes.SettlorIndividualAddressInternationalController.onPageLoad(NormalMode, index, draftId)
     )
-    case SettlorAddressUKPage(index) => _ => _ =>
-      controllers.living_settlor.individual.routes.SettlorIndividualPassportYesNoController.onPageLoad(NormalMode, index, draftId)
-    case SettlorAddressInternationalPage(index) => _ => _ =>
-      controllers.living_settlor.individual.routes.SettlorIndividualPassportYesNoController.onPageLoad(NormalMode, index, draftId)
     case SettlorIndividualPassportYesNoPage(index)  => _ => yesNoNav(
       fromPage = SettlorIndividualPassportYesNoPage(index),
       yesCall = controllers.living_settlor.individual.routes.SettlorIndividualPassportController.onPageLoad(NormalMode, index, draftId),
       noCall = controllers.living_settlor.individual.routes.SettlorIndividualIDCardYesNoController.onPageLoad(NormalMode, index, draftId)
     )
-    case SettlorIndividualPassportPage(index) => _ => _ =>
-      controllers.living_settlor.individual.routes.SettlorIndividualAnswerController.onPageLoad(index, draftId)
     case SettlorIndividualIDCardYesNoPage(index)  => _ => yesNoNav(
       fromPage = SettlorIndividualIDCardYesNoPage(index),
       yesCall = controllers.living_settlor.individual.routes.SettlorIndividualIDCardController.onPageLoad(NormalMode, index, draftId),
       noCall = controllers.living_settlor.individual.routes.SettlorIndividualAnswerController.onPageLoad(index, draftId)
     )
-    case SettlorIndividualIDCardPage(index) => _ => _ =>
-      controllers.living_settlor.individual.routes.SettlorIndividualAnswerController.onPageLoad(index, draftId)
-    case SettlorIndividualAnswerPage => _ => _ =>
-      controllers.routes.AddASettlorController.onPageLoad(draftId)
   }
 }
