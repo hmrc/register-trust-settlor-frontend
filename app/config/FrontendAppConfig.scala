cfbb0f575c9ef8c861929f5689e498bc562bda63
Rajdeep Vijayakar <rajdeep.vijayakar@digital.hmrc.gov.uk>
2021-02-09 09:35:07 +0000

    TRUS-3578: Pass N/A as analytics token

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -37,7 +37,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val repositoryKey: String = "settlors"
 
   val appName: String = configuration.get[String]("appName")
-  val analyticsHost: String = configuration.get[String](s"google-analytics.host")
+  val analyticsToken: String = configuration.get[String](s"google-analytics.token")
 
   val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
   val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
