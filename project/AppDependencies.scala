2b18731c931f50a43446ff10004a1e0b7d012f79
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2021-03-26 14:38:28 +0000

    Upgrade| upgraded last appDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo"   %% "play2-reactivemongo"            % "0.18.8-play27",
-    "uk.gov.hmrc"         %% "govuk-template"                 % "5.60.0-play-27",
-    "uk.gov.hmrc"         %% "play-health"                    % "3.15.0-play-27",
-    "uk.gov.hmrc"         %% "play-ui"                        % "8.20.0-play-27",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.5.0-play-27",
+    "org.reactivemongo"   %% "play2-reactivemongo"            % "0.20.3-play27",
+    "uk.gov.hmrc"         %% "govuk-template"                 % "5.63.0-play-27",
+    "uk.gov.hmrc"         %% "play-health"                    % "3.16.0-play-27",
+    "uk.gov.hmrc"         %% "play-ui"                        % "9.0.0-play-27",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.6.0-play-27",
     "uk.gov.hmrc"         %% "domain"                         % "5.10.0-play-27",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-27"     % "4.1.0",
     "uk.gov.hmrc"         %% "play-language"                  % "4.10.0-play-27"
