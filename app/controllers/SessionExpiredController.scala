9e7e46d290e45e1213f24c26a97bc1b1a9a57e76
carrielies <carrielies@users.noreply.github.com>
2021-06-16 15:49:11 +0100

    TRUS-4277 Fix View Imports

--- app/controllers/SessionExpiredController.scala
+++ app/controllers/SessionExpiredController.scala
@@ -16,15 +16,16 @@
 
 package controllers
 
+import config.FrontendAppConfig
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.SessionExpiredView
-
 import javax.inject.Inject
 
 class SessionExpiredController @Inject()(
                                           val controllerComponents: MessagesControllerComponents,
+                                          appConfig: FrontendAppConfig,
                                           view: SessionExpiredView
                                         ) extends FrontendBaseController with I18nSupport {
 
