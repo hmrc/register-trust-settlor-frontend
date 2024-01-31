8e356f66a1300b038e7ec63bfa53923b20f8b13f
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2024-01-31 16:04:06 +0000

    DDCE-5067: Update to stop form submission with smart apostrophes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.22.0"
+  val bootstrapVersion = "7.23.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28"    % bootstrapVersion,
     "uk.gov.hmrc" %% "domain"                        % "8.3.0-play-28",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"            % "7.23.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"            % "7.29.0-play-28",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28"
   )
 
@@ -17,11 +17,11 @@ object AppDependencies {
     "org.scalatest"          %% "scalatest"                % "3.2.17",
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
-    "org.jsoup"               % "jsoup"                    % "1.16.1",
-    "org.mockito"            %% "mockito-scala-scalatest"  % "1.17.27",
+    "org.jsoup"               % "jsoup"                    % "1.17.2",
+    "org.mockito"            %% "mockito-scala-scalatest"  % "1.17.30",
     "wolfendale"             %% "scalacheck-gen-regexp"    % "0.1.2",
     "org.scalacheck"         %% "scalacheck"               % "1.17.0",
-    "org.wiremock"            % "wiremock-standalone"      % "3.2.0",
+    "org.wiremock"            % "wiremock-standalone"      % "3.3.1",
     "com.vladsch.flexmark"    % "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
 
