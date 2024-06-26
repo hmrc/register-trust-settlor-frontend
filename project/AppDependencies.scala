b3c2b6b125ce9a63e2d232efef5be86a00b38d65
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2024-06-26 12:31:20 +0100

    DDCE-5552: Initial Play upgrade changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.5.0"
+  private val bootstrapVersion = "9.0.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc" %% "domain-play-30"                        % "9.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "8.5.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "9.11.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "2.0.0"
   )
 
